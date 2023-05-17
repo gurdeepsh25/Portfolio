8e71e4e584f25f9bc33d6fff297d68aa2c55b862
Gurdeep Singh <127475185+gurdeepsh25@users.noreply.github.com>
2023-05-17 09:24:15 +0530

    Implemented Change Background Header in Portfolio.

--- js/main.js
+++ js/main.js
@@ -147,3 +147,12 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
+
+/* ====== Change Background Header ====== */
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport the height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
