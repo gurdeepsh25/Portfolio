239b5fbdef20daec80af679e7b41966bedced4c1
Gurdeep Singh <127475185+gurdeepsh25@users.noreply.github.com>
2023-05-17 09:47:10 +0530

    Implemented Show Scroll Up Button using JS in Portfolio.

--- js/main.js
+++ js/main.js
@@ -156,3 +156,12 @@ function scrollHeader() {
   else nav.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
+
+/* ====== Show Scroll Up ====== */
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
